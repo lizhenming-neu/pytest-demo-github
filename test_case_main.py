074326c684f930d68a0f128f77d6fcdea0196cf3
lizhenming <15040123966@163.com>
2022-06-05 12:15:53 +0800

    Initial commit

---
+++ test_case_main.py
@@ -0,0 +1,26 @@
+import os
+
+import json
+import datetime
+import time
+import os
+import shutil
+import pytest
+
+
+def test_main():
+    # path = "tests/"
+    # case_names = os.listdir(path)
+    # case_name_temp=[]
+    # case_name_temp.append(case_names[0])
+    # case_name_temp.append(case_names[1])
+    # for case_name in case_name_temp:
+    #     print("tests name:",case_name)
+    #     start_case(case_name)
+
+    assert 1 == 1
+
+
+if __name__ == '__main__':
+    pytest.main()
+    # start_test()
